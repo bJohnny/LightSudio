440665bf24bd76ea7d66449ce1d118627fddbec9
bJohnny <der.jbrown@gmail.com>
2016-06-11 14:21:11 +0200

    Inital commit

---
+++ Core/Tutorial.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+using Fusee.Xene;
+using static Fusee.Engine.Core.Input;
+
+namespace Fusee.Tutorial.Core
+{
+
+    class Renderer : SceneVisitor
+    {
+        public RenderContext RC;
+        public IShaderParam AlbedoParam;
+        public IShaderParam ShininessParam;
+        public float4x4 View;
+        private Dictionary<MeshComponent, Mesh> _meshes = new Dictionary<MeshComponent, Mesh>();
+        private CollapsingStateStack<float4x4> _model = new CollapsingStateStack<float4x4>();
+        private Mesh LookupMesh(MeshComponent mc)
+        {
+            Mesh mesh;
+            if (!_meshes.TryGetValue(mc, out mesh))
+            {
+                mesh = new Mesh
+                {
+                    Vertices = mc.Vertices,
+                    Normals = mc.Normals,
+                    Triangles = mc.Triangles
+                };
+                _meshes[mc] = mesh;
+            }
+            return mesh;
+        }
+
+        public Renderer(RenderContext rc)
+        {
+            RC = rc;
+            // Initialize the shader(s)
+            var vertsh = AssetStorage.Get<string>("VertexShader.vert");
+            var pixsh = AssetStorage.Get<string>("PixelShader.frag");
+            var shader = RC.CreateShader(vertsh, pixsh);
+            RC.SetShader(shader);
+            AlbedoParam = RC.GetShaderParam(shader, "albedo");
+            ShininessParam = RC.GetShaderParam(shader, "shininess");
+        }
+
+        protected override void InitState()
+        {
+            _model.Clear();
+            _model.Tos = float4x4.Identity;
+        }
+        protected override void PushState()
+        {
+            _model.Push();
+        }
+        protected override void PopState()
+        {
+            _model.Pop();
+            RC.ModelView = View * _model.Tos;
+        }
+        [VisitMethod]
+        void OnMesh(MeshComponent mesh)
+        {
+            RC.Render(LookupMesh(mesh));
+        }
+        [VisitMethod]
+        void OnMaterial(MaterialComponent material)
+        {
+            RC.SetShaderParam(AlbedoParam, material.Diffuse.Color);
+            RC.SetShaderParam(ShininessParam, material.Specular.Shininess);
+        }
+        [VisitMethod]
+        void OnTransform(TransformComponent xform)
+        {
+            _model.Tos *= xform.Matrix();
+            RC.ModelView = View * _model.Tos;
+        }
+    }
+
+
+    [FuseeApplication(Name = "Tutorial Example", Description = "The official FUSEE Tutorial.")]
+    public class Tutorial : RenderCanvas
+    {
+        private Mesh _mesh;
+        private TransformComponent _wheelBigL;
+
+        private IShaderParam _albedoParam;
+        private float _alpha = 0.001f;
+        private float _beta;
+
+        private SceneOb _root;
+        private SceneContainer _wuggy;
+
+        private SceneContainer _sphere;
+
+        private Renderer _renderer;
+
+        // Init is called on startup. 
+        public override void Init()
+        {
+            // Load some meshes
+            _wuggy = AssetStorage.Get<SceneContainer>("wuggy.fus");
+            _wheelBigL = _wuggy.Children.FindNodes(n => n.Name == "WheelBigL").First().GetTransform();
+            _sphere = AssetStorage.Get<SceneContainer>("Sphere.fus");
+            
+            _renderer = new Renderer(RC);
+
+            // Set the clear color for the backbuffer
+            RC.ClearColor = new float4(1, 1, 1, 1);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            float2 speed = Mouse.Velocity + Touch.GetVelocity(TouchPoints.Touchpoint_0);
+            if (Mouse.LeftButton || Touch.GetTouchActive(TouchPoints.Touchpoint_0))
+            {
+                _alpha -= speed.x * 0.0001f;
+                _beta -= speed.y * 0.0001f;
+            }
+
+            _wheelBigL.Rotation += new float3(-0.05f * Keyboard.WSAxis, 0, 0);
+
+            // Setup matrices
+            var aspectRatio = Width / (float)Height;
+            RC.Projection = float4x4.CreatePerspectiveFieldOfView(3.141592f * 0.25f, aspectRatio, 0.01f, 20);
+            float4x4 view = float4x4.CreateTranslation(0, 0, 5) * float4x4.CreateRotationY(_alpha) * float4x4.CreateRotationX(_beta) * float4x4.CreateTranslation(0, -0.5f, 0);
+            _renderer.View = view;
+            //_renderer.Traverse(_wuggy.Children);
+            _renderer.Traverse(_sphere.Children);
+
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
+            Present();
+        }
+
+
+        // Is called when the window was resized
+        public override void Resize()
+        {
+            // Set the new rendering area to the entire new windows size
+            RC.Viewport(0, 0, Width, Height);
+
+            // Create a new projection matrix generating undistorted images on the new aspect ratio.
+            var aspectRatio = Width / (float)Height;
+
+            // 0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
+            // Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
+            // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
+            var projection = float4x4.CreatePerspectiveFieldOfView(3.141592f * 0.25f, aspectRatio, 1, 20000);
+            RC.Projection = projection;
+        }
+
+    }
+}
